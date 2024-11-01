e6b0cb51a0b0fa3a5c813711c5feeb4dd856a297
benjGam <mecopilive@gmail.com>
2024-11-01 17:17:51 +0100

    feat: add way to check status

--- easy-commit.sh
+++ easy-commit.sh
@@ -4,14 +4,24 @@ read -e -p "Commit message prefix: " preffix
 while true
 do
   read -e -p "Commit message: " msg
+
   if [ "$msg" == "exit" ]
   then
     exit 0
   fi
+
+  if [ "$msg" == "gst" ]
+  then
+    git status
+    read -n 1 -p "Waiting for input"
+    echo ""
+  fi
+
+  git commit -am "$preffix: $msg"
+  sleep 1
+
   if [ "$clearEnabled" != 'n' -a "$clearEnabled" != 'N' ]
   then
     clear
   fi
-
-  git commit -am "$preffix: $msg"
 done
