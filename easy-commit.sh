21a63ece325564f449dd70e2e39574219f790a6e
benjGam <mecopilive@gmail.com>
2024-11-01 17:08:45 +0100

    feat: implement auto clear after commiting

--- easy-commit.sh
+++ easy-commit.sh
@@ -1,11 +1,16 @@
 # /bin/sh
+read -n 1 -p "Clear between each commit ? [Y/n]: " clearEnabled
 read -e -p "Commit message prefix: " preffix
 while true
 do
-  read -e -p "Message: " msg
+  read -e -p "Commit message: " msg
   if [ "$msg" == "exit" ]
   then
     exit 0
+  fi
+  if [ "$clearEnabled" != 'n' -a "$clearEnabled" != 'N' ]
+  then
+    clear
   fi
 
   git commit -am "$preffix: $msg"
